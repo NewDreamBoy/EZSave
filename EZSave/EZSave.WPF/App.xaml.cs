70589c2998af20626fbc78353bc592a56082aae8
New_Dream <aa25136211loldnf@gmail.com>
2024-09-05 13:11:56 +0800

    引入Microsoft.Extensions.Logging，日志模块基础搭建

--- EZSave/EZSave.WPF/App.xaml.cs
+++ EZSave/EZSave.WPF/App.xaml.cs
@@ -3,6 +3,10 @@ using EZSave.WPF.Views;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace EZSave.WPF
 {
@@ -13,21 +17,49 @@ namespace EZSave.WPF
     {
         public static IServiceProvider ServiceProvider { get; private set; }
 
+        private IHost _host;
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            var services = new ServiceCollection();
-            ConfigureServices(services);
-            ServiceProvider = services.BuildServiceProvider();
-            var initialGuidanceWindow = ServiceProvider.GetRequiredService<InitialGuidanceWindow>();
+            AppInit();
+            ServiceProvider = _host.Services;
+            var initialGuidanceWindow = _host.Services.GetRequiredService<InitialGuidanceWindow>();
             initialGuidanceWindow.Show();
-
             base.OnStartup(e);
         }
 
-        private void ConfigureServices(ServiceCollection services)
+        private void AppInit()
         {
-            services.AddViewModels(Assembly.GetExecutingAssembly());
-            services.AddTransient<InitialGuidanceWindow>();
+            _host = Host.CreateDefaultBuilder().ConfigureAppConfiguration((context, config) =>
+            {
+                //获取应用程序根目录
+                var basePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\.."));
+                //设置配置文件目录
+                config.SetBasePath(basePath);
+                //获取当前运行环境
+                var env = context.HostingEnvironment;
+                //加载配置文件
+                config.AddJsonFile($"Configurations/appsettings.{Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}.json", optional: true, reloadOnChange: true);
+
+            }).ConfigureServices((context, services) =>
+            {
+                //注入服务
+                services.AddViewModels(Assembly.GetExecutingAssembly());
+                services.AddTransient<InitialGuidanceWindow>();
+
+            }).ConfigureLogging((context, logging) =>
+            {
+                // 从配置文件加载日志配置
+                logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                if (Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "Development")
+                {
+                    logging.AddConsole(); // 在开发环境中输出到控制台
+                }
+                else
+                {
+                    
+                }
+            }).Build();
         }
     }
 }
--- EZSave/EZSave.WPF/ViewModels/InitialGuidanceWindowViewModel.cs
+++ EZSave/EZSave.WPF/ViewModels/InitialGuidanceWindowViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
 
 namespace EZSave.WPF.ViewModels
 {
     public partial class InitialGuidanceWindowViewModel : ViewModelBase
     {
+
+        private readonly ILogger<InitialGuidanceWindowViewModel> _logger;
+
+        public InitialGuidanceWindowViewModel(ILogger<InitialGuidanceWindowViewModel> logger)
+        {
+            this._logger = logger;
+            _logger.LogDebug("LogDebug 应该不输出把");
+        }
+
         [ObservableProperty]
         private string _title = "引导窗口";
 
